149a5932345626b5c615515a026a306977657abc
Amohrs20 <amohrs@miumg.edu.gt>
2022-09-09 20:58:33 -0600

    primer commit

---
+++ docs/Produccion.sql
@@ -0,0 +1,120 @@
+CREATE TABLE `tm_usuario` (
+  `usu_id` int(11) NOT NULL,
+  `usu_nom` varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `usu_ape` varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `usu_correo` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
+  `usu_pass` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
+  `rol_id` int(11) DEFAULT NULL,
+  `fech_crea` datetime DEFAULT NULL,
+  `fech_modi` datetime DEFAULT NULL,
+  `fech_elim` datetime DEFAULT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Tabla Mantenedor de Usuarios';
+
+INSERT INTO `tm_usuario` (`usu_id`, `usu_nom`, `usu_ape`, `usu_correo`, `usu_pass`, `rol_id`, `fech_crea`, `fech_modi`, `fech_elim`, `est`) VALUES
+(1, 'Anner', 'Delgado', 'adelgadon@outlook.com', '1234', 1, '2022-05-19', NULL, NULL, 1);
+
+CREATE TABLE `tm_ticket` (
+  `tick_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `tick_titulo` varchar(250) COLLATE utf8_spanish_ci NOT NULL,
+  `tick_descrip` mediumtext COLLATE utf8_spanish_ci NOT NULL,
+  `tick_estado` varchar(15) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `fech_crea` datetime DEFAULT NULL,
+  `usu_asig` int(11) DEFAULT NULL,
+  `fech_asig` datetime DEFAULT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+
+CREATE TABLE `tm_categoria` (
+  `cat_id` int(11) NOT NULL,
+  `cat_nom` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+INSERT INTO `tm_categoria` (`cat_id`, `cat_nom`, `est`) VALUES
+(1, 'Telefonía', 1),
+(2, 'Computo', 1),
+(3, 'Navegación', 1),
+(4, 'Permisos a sistema', 1),
+(5, 'Correo electrónico', 1),
+(6, 'Falla en portales', 1),
+(7, 'Instalación de software', 1),
+(8, 'Licencias', 1),
+(9, 'Antivirus', 1),
+(10, 'Otras', 1);
+
+CREATE TABLE `td_ticketdetalle` (
+  `tickd_id` int(11) NOT NULL,
+  `tick_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL,
+  `tickd_descrip` mediumtext COLLATE utf8_spanish_ci NOT NULL,
+  `fech_crea` datetime NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+CREATE TABLE `td_documento` (
+  `doc_id` int(11) NOT NULL,
+  `tick_id` int(11) NOT NULL,
+  `doc_nom` varchar(400) COLLATE utf8_spanish_ci NOT NULL,
+  `fech_crea` datetime NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+ALTER TABLE `td_documento`
+  ADD PRIMARY KEY (`doc_id`);
+
+--
+-- Indices de la tabla `td_ticketdetalle`
+--
+ALTER TABLE `td_ticketdetalle`
+  ADD PRIMARY KEY (`tickd_id`);
+
+--
+-- Indices de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indices de la tabla `tm_ticket`
+--
+ALTER TABLE `tm_ticket`
+  ADD PRIMARY KEY (`tick_id`);
+
+--
+-- Indices de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  ADD PRIMARY KEY (`usu_id`);
+
+
+
+ALTER TABLE `td_documento`
+  MODIFY `doc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `td_ticketdetalle`
+--
+ALTER TABLE `td_ticketdetalle`
+  MODIFY `tickd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_ticket`
+--
+ALTER TABLE `tm_ticket`
+  MODIFY `tick_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
